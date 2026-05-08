13c0cc18d00b4d2bcbcb6e8c65438e747b9bc880
gblcezimbra-netizen <gblcezimbra@gmail.com>
2026-05-08 18:19:38 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Plataforma de Estudos
+
+## Login Admin
+Email: admin@mathplatform.com
+Senha: Czb2004#
+
+## Instalação
+
+```bash
+npm install
+npm run dev
+```
+
+## Deploy recomendado
+- Vercel
+- Supabase
+
+## Estrutura
+- /app -> páginas
+- /components -> componentes
+- /prisma -> banco de dados
---
+++ page.tsx
@@ -0,0 +1,19 @@
+
+export default function Dashboard() {
+  return (
+    <main style={{padding:40,fontFamily:"Arial"}}>
+      <h1>Dashboard</h1>
+
+      <div style={{
+        border:"1px solid #ccc",
+        padding:20,
+        borderRadius:10,
+        marginTop:20
+      }}>
+        <h2>Cálculo III</h2>
+        <p>Integrais Duplas</p>
+      </div>
+
+    </main>
+  )
+}
